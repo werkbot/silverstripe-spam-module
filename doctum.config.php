1ccabe360180bb75716356d0ec1d964035f0c576
Tyler <tiller1010@gmail.com>
2023-12-15 09:37:04 -0500

    feat: psr-4, psr-12, and doctum

---
+++ doctum.config.php
@@ -0,0 +1,8 @@
+<?php
+
+return new Doctum\Doctum(__DIR__ . '/src', [
+  'title' => 'SilverStripe Spam Module',
+  'build_dir' => __DIR__.'/doctum_build',
+  'cache_dir' => __DIR__.'/doctum_cache',
+]);
+
