d898e7f81c8883064e73101e98946379500561ff
Tyler Trout <tiller1010@gmail.com>
2024-08-13 09:42:00 -0400

    feat: psr-4, psr-12, and doctum (#11)

--- src/Forms/EditableTimerField.php
+++ src/Forms/EditableTimerField.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Werkbot\SpamProtection;
+namespace Werkbot\SpamProtection\Forms;
 
 use Werkbot\SpamProtection\TimerField;
 use SilverStripe\Forms\FormField;
@@ -12,45 +12,49 @@ use SilverStripe\Core\Injector\Injector;
 
 class EditableTimerField extends EditableFormField
 {
-  /**/
-    private static $singular_name = 'Timer Spam Protection Field';
-    private static $plural_name = 'Timer Spam Protection Fields';
-    private static $table_name = 'EditableTimerField';
-  /**/
+  private static $singular_name = 'Timer Spam Protection Field';
+  private static $plural_name = 'Timer Spam Protection Fields';
+  private static $table_name = 'EditableTimerField';
+
   private static $db = [
     'TimeNotABot' => 'Int'
   ];
+
   /**
    * @var FormField
    */
-    protected $formField = null;
-   /**/
-   public function getFormField(){
+  protected $formField = null;
+
+  public function getFormField()
+  {
     // Clear Any existing errors
     $Request = Injector::inst()->get(HTTPRequest::class);
     $Session = $Request->getSession();
     $Session->clear('spam-protection-error-exists');
-    //
+
     $field = TimerField::create($this->Name, $this->Title, time())->setFieldHolderTemplate('Form\\SpamFieldHolder');
     $this->doUpdateFormField($field);
-    //
+
     return $field;
   }
+
   /**
    * @param FormField $field
    * @return self
    */
-    public function setFormField(FormField $field)
-    {
-        $this->formField = $field;
-        return $this;
-    }
+  public function setFormField(FormField $field)
+  {
+    $this->formField = $field;
+    return $this;
+  }
+
   /**
    * Used in userforms 3.x and above
    *
    * {@inheritDoc}
    */
-  public function getCMSFields() {
+  public function getCMSFields()
+  {
     $this->beforeUpdateCMSFields(function ($fields) {
       $fields->addFieldsToTab(
         'Root.Main',
@@ -62,9 +66,10 @@ class EditableTimerField extends EditableFormField
         ]
       );
     });
-    //
+
     return parent::getCMSFields();
   }
+
   /**
    * Updates a formfield with the additional metadata specified by this field
    *
@@ -73,23 +78,27 @@ class EditableTimerField extends EditableFormField
   protected function updateFormField($field)
   {
     parent::updateFormField($field);
+
     // Add custom time
     if (is_numeric($this->TimeNotABot) && $this->TimeNotABot > 0) {
       $field->setAttribute('data-rule-customtime', (int) $this->TimeNotABot);
     }
+
     // Add custom error
     if ($this->CustomErrorMessage <> ""){
       $field->setAttribute('data-custommsg', (string) $this->CustomErrorMessage);
     }
   }
-  /**/
-    public function getRequired()
-    {
-        return false;
-    }
-  /**/
-    public function showInReports()
-    {
-        return false;
-    }
+
+  public function getRequired()
+  {
+    return false;
+  }
+
+  public function showInReports()
+  {
+    return false;
+  }
+
 }
+
--- src/Forms/TimerField.php
+++ src/Forms/TimerField.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Werkbot\SpamProtection;
+namespace Werkbot\SpamProtection\Forms;
 
 use SilverStripe\Forms\TextField;
 use SilverStripe\Control\HTTPRequest;
@@ -11,62 +11,65 @@ class TimerField extends TextField
   /**
    * @config
    */
-    private static $time_not_bot = 10;
+  private static $time_not_bot = 10;
   /**
    * Adds in the requirements for the field
    * @param array $properties Array of properties for the form element (not used)
    * @return string Rendered field template
    */
-    public function Field($properties = array())
-    {
-        return parent::Field($properties);
-    }
-  /**/
-    public function validate($validator)
-    {
-      $Attributes = $this->getAttributes();
-      $Request = Injector::inst()->get(HTTPRequest::class);
-      $Session = $Request->getSession();
-      //
-        $Timer = ((isset($Attributes['data-rule-customtime'])) ? $Attributes['data-rule-customtime'] : (($this->config()->time_not_bot) ? $this->config()->time_not_bot : $this->time_not_bot));
-        $CurrentTime = time();
-      // Compare time difference with allowed time difference
-        if ((empty($this->Value()) || !is_numeric($this->Value()) || (($CurrentTime - $this->Value()) < $Timer))) {
-          if (!$Session->get('spam-protection-error-exists')) {
-            // Set Session
-            $Session->set('spam-protection-error-exists', true);
-            //
-            if(isset($Attributes['data-custommsg']) && $Attributes['data-custommsg'] <> ""){
-              $validator->validationError(
-                $this->Name,
-                $Attributes['data-custommsg']
-              );
-            } else {
-              $validator->validationError(
-                $this->Name,
-                _t('Werkbot\SpamProtection\Timer.INVALID', 'There was an error submitting this form. Please try again.')
-              );
-            }
-          }
-          return false;
+  public function Field($properties = array())
+  {
+    return parent::Field($properties);
+  }
+
+  public function validate($validator)
+  {
+    $Attributes = $this->getAttributes();
+    $Request = Injector::inst()->get(HTTPRequest::class);
+    $Session = $Request->getSession();
+
+    $Timer = ((isset($Attributes['data-rule-customtime'])) ? $Attributes['data-rule-customtime'] : (($this->config()->time_not_bot) ? $this->config()->time_not_bot : $this->time_not_bot));
+    $CurrentTime = time();
+
+    // Compare time difference with allowed time difference
+    if ((empty($this->Value()) || !is_numeric($this->Value()) || (($CurrentTime - $this->Value()) < $Timer))) {
+      if (!$Session->get('spam-protection-error-exists')) {
+        // Set Session
+        $Session->set('spam-protection-error-exists', true);
+
+        if(isset($Attributes['data-custommsg']) && $Attributes['data-custommsg'] <> ""){
+          $validator->validationError(
+            $this->Name,
+            $Attributes['data-custommsg']
+          );
+        } else {
+          $validator->validationError(
+            $this->Name,
+            _t('Werkbot\SpamProtection\Timer.INVALID', 'There was an error submitting this form. Please try again.')
+          );
         }
-      //
-        return true;
+      }
+      return false;
     }
+
+    return true;
+  }
+
   /**
    * @return array
    */
-    public function getAttributes()
-    {
-      //
-        $attributes = [
-        'class' => 'wb-spam-hidden',
-        'style' => 'height: 1px; opacity: 0; margin: 0;'
-        ];
-      //
-        return array_merge(
-            parent::getAttributes(),
-            $attributes
-        );
-    }
+  public function getAttributes()
+  {
+    $attributes = [
+      'class' => 'wb-spam-hidden',
+      'style' => 'height: 1px; opacity: 0; margin: 0;'
+    ];
+
+    return array_merge(
+      parent::getAttributes(),
+      $attributes
+    );
+  }
+
 }
+
